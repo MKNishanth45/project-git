a4fdc34bcb9197d6d252edf78e5f4efbd81719fb
MKNishanth45 <nishanthkaruppusamy54@gmail.com>
2025-02-17 10:58:44 +0530

    update

---
+++ area rectangle.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+int length,breadth,area;
+printf("enter the value of length:");
+scanf("%d",&length);
+printf("enter the value of breadth:");
+scanf("%d",&breadth);
+area=length*breadth;
+printf("The Area of rectangle is %d.",area);
+
+
+return 0;
+}
